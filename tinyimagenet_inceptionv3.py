8554ae5ce4d8f2c913e38ab5534eff54d985042a
Jisoo Lee <swang.jislee@gmail.com>
2020-12-14 17:04:12 +0900

    add tiny imagenet

--- tinyimagenet_inceptionv3.py
+++ tinyimagenet_inceptionv3.py
@@ -1,5 +1,7 @@
 #####
 # dataset: tinyImageNet
+# All images are of size 64×64; 200 image classes
+# Training = 100,000 images; Validation = 10,000 images; Test = 10,000 images.
 
 # model: inception v3
 # ref.
@@ -31,42 +33,63 @@
 # RMSProp Optimizer; Factorized 7x7 convolutions; BatchNorm in the Auxillary Classifiers; Label Smoothing
 
 # Import necessary libraries
+import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.layers import ZeroPadding2D, Conv2D, Activation, BatchNormalization, MaxPooling2D, GlobalAveragePooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import ZeroPadding2D, Conv2D, Activation, BatchNormalization, MaxPooling2D, GlobalAveragePooling2D, Flatten, Dense
 
 # Fix the gpu memory issue
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3  # <=0.3 with GTX 1050 (2GB)
 session = tf.compat.v1.Session(config=config)
 
-# Load tiny imangenet dataset
-cifar10 = tf.keras.datasets.cifar10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-# Normalize the dataset
-x_train, x_test = x_train / 255.0, x_test / 255.0
-x_train = x_train.astype("float32")
-x_test = x_test.astype("float32")
-
 # Set the value of hyper-parameters
-upsampling_size = (2,2)
+upsampling_size = (2, 2)
 learning_rate = 0.0001
 epochs = 20
 batch_size = 16
+
+# Load tiny imangenet dataset
+
+# To download the imagenet, you may need to install wget and unzip first.
+# For installation, check here:
+# https://builtvisible.com/download-your-website-with-wget/
+# https://zoomadmin.com/HowToInstall/UbuntuPackage/unzip
+# On terminal, run the codes below:
+# wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+# unzip -qq 'tiny-imagenet-200.zip'
+
+path = 'C:/wgetdown/tiny-imagenet-200'
+
+val_data = pd.read_csv('./tiny-imagenet-200/val/val_annotations.txt', sep='\t', header=None,
+                       names=['File', 'Class', 'X', 'Y', 'H', 'W'])
+val_data.drop(['X', 'Y', 'H', 'W'], axis=1, inplace=True)
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+valid_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(r'./tiny-imagenet-200/train/', target_size=(64, 64),
+                                                    color_mode='rgb', class_mode='categorical',
+                                                    batch_size=batch_size, shuffle=True, seed=42)
+valid_generator = valid_datagen.flow_from_dataframe(val_data, directory='./tiny-imagenet-200/val/images/',
+                                                    x_col='File', y_col='Class', target_size=(64, 64),
+                                                    color_mode='rgb', class_mode='categorical',
+                                                    batch_size=batch_size, shuffle=True, seed=42)
 
 # Results by hyper-parameters
 # ==> upsampling: 2; learing rate: 0.0001; epoch: 20; batch size: 16;
 
 # Initiate a ResNet50 architecture
-input_tensor = Input(shape=(75, 75, 3), dtype='float32', name='input')  # 32,32,3
+input_tensor = Input(shape=(64, 64, 3), dtype='float32', name='input')
 # Rescale image (up-sampling) for better performance
 upsampling = tf.keras.layers.UpSampling2D(size=upsampling_size, name='upsampling')(input_tensor)
 
 # conv1
-
-
-
+conv1 = Conv2D(32, 3, strides=(2, 2), name='conv1')(upsampling)
+conv1 = Conv2D(32, 3, padding='SAME', name='conv1')(conv1)
+conv1_bn = BatchNormalization(axis=1, name='conv1_bn')(x)
+x = Conv2D(64, 3, padding='SAME', name='conv1')(conv1_bn)
 
 '''
 # tf.keras module
