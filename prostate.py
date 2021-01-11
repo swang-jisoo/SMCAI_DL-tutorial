a62a0c8a1fc3f57c3fc17636e3484004feaa3779
swang.jislee <swang.jislee@gmail.com>
2021-01-11 11:24:40 +0900

    edit fn load_images()

--- prostate.py
+++ prostate.py
@@ -21,6 +21,8 @@ from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Cropping2D, concatenate
 
 # Hyper-parameters
+ori_size = (216, 216)
+img_size = (72, 72)
 learning_rate = 0.05  # for u-net, start with larger learning rate
 batch_size = 16
 epochs = 20
@@ -48,6 +50,51 @@ test_tar = ['Gleason_masks_test_pathologist1',  # 'Gleason_masks_test_pathologis
 
 data_dir = 'D:/PycharmProjects/dataverse_files'
 
+
+def load_images(is_train, ori_size, img_size):
+    if is_train:
+        tar = train_tar
+    else:
+        tar = test_tar
+
+    mask_tar, img_tar, mask_fname, img_fname = get_tar_fname(data_dir, tar)
+
+    xs, ys = [], []
+
+    for m in range(len(mask_fname)):
+        tar_idx = tar.index(os.path.dirname(img_fname[m])) - 1  # excl. train_tar[0] which is the mask folder
+        x = img_tar[tar_idx].extractfile(img_fname[m])
+        y = mask_tar.extractfile(mask_fname[m])
+
+        x = Image.open(x)
+        y = Image.open(y)
+
+        # Preprocessing (crop and normalize)
+        # *** randomly crop -> resize, increase sample size
+        width, height = x.size
+        cx = int(width / 2)
+        cy = int(height / 2)
+
+        roi = (cx - ori_size[0] / 2, cy - ori_size[1] / 2, cx + ori_size[0] / 2, cy + ori_size[1] / 2)
+
+        x = x.crop(roi)
+        y = y.crop(roi)
+
+        x = x.resize(img_size)
+        y = y.resize(img_size)
+
+        x = np.asarray(x, dtype='float32') / 255.0
+        y = np.asarray(y, dtype='float32') / 255.0
+
+        xs.append(x)
+        ys.append(y)
+
+    x_np = np.asarray(xs)
+    y_np = np.asarray(ys)
+
+    return x_np, y_np
+
+
 def get_tar_fname(data_dir, tar):
     img_tar, img_fname = [], []
     for t in range(len(tar)):
@@ -58,164 +105,69 @@ def get_tar_fname(data_dir, tar):
         else:
             i_tar = tarfile.open(tar_fname)
             img_tar.append(i_tar)
-            img_fname.append(i_tar.getnames()[1:])
+            img_fname += i_tar.getnames()[1:]
+    mask_fname, img_fname = match_fname(mask_fname, img_fname)
     return mask_tar, img_tar, mask_fname, img_fname
 
-mask_tar, img_tar, mask_fname, img_fname = get_tar_fname(data_dir, train_tar)
 
-mask_fname_match, img_fname_match = [], []
+def match_fname(mask_fname, img_fname):
+    mask_fname_match, img_fname_match = [], []
+    for m in mask_fname:
+        m_fname_base = os.path.splitext(os.path.basename(m))[0]
+        m_fname_split = m_fname_base.split('_')
+        if 'mask' not in m_fname_split[0]:
+            # Skip mask file names starting with '.mask'
+            continue
+        matcher = '_'.join(m_fname_split[1:4]) + '/' + '_'.join(m_fname_split[1:]) + '.jpg'
+        for i in img_fname:
+            if matcher == i:
+                mask_fname_match.append(m)
+                img_fname_match.append(i)
+    return mask_fname_match, img_fname_match
 
-for m in range(len(mask_fname)):
-    m_fname = os.path.splitext(os.path.basename(mask_fname[m]))[0]
-    m_fname_split = m_fname.split('_')
-    if 'mask' not in m_fname_split[0]:
-        continue
-    tar_idx = train_tar.index('_'.join(m_fname_split[1:4]))
-    for i in range(len(img_fname[tar_idx - 1])):
-        i_fname = os.path.splitext(os.path.basename(img_fname[tar_idx - 1][i]))[0]
-        if '_'.join(m_fname_split[1:]) == i_fname:
-            mask_fname_match.append(mask_fname[m])
-            img_fname_match.append(img_fname[tar_idx - 1][i])
 
 '''
-# 수정 중
-
 def get_tar_fname(data_dir, tar):
+    """
+    """
     img_tar, img_fname = [], {}
     for t in range(len(tar)):
         tar_fname = os.path.join(data_dir, tar[t] + '.tar.gz')
         if t == 0:
             mask_tar = tarfile.open(tar_fname)
-            mask_fname = mask_tar.getnames()
+            mask_fname = mask_tar.getnames()[1:]
+            mask_fname = get_mask_fname_dict(mask_fname)
         else:
             i_tar = tarfile.open(tar_fname)
             img_tar.append(i_tar)
-            i_fname = i_tar.getnames()
-            img_fname[i_fname[0]] = i_fname[1:]
+            img_fname[tar[t]] = i_tar.getnames()[1:]
     return mask_tar, img_tar, mask_fname, img_fname
 
+def get_mask_fname_dict(mask_fname):
+    """
+    Convert the list of mask file names into a dictionary with its corresponding image folder name as a key.
+    """
+    mask_fname_dict = {}
+    for m in range(len(mask_fname)):
+        m_fname_base = os.path.splitext(os.path.basename(mask_fname[m]))[0]
+        m_fname_split = m_fname_base.split('_')
+        if 'mask' not in m_fname_split[0]:
+            # Skip mask file names starting with '.mask'
+            continue
+        tar_key = '_'.join(m_fname_split[1:4])
+        # Save the mask file names in dictionary
+        if tar_key in mask_fname_dict.keys():
+            mask_fname_dict[tar_key].append(mask_fname[m])
+        else:
+            mask_fname_dict[tar_key] = [mask_fname[m]]
+    return mask_fname_dict
+
 mask_tar, img_tar, mask_fname, img_fname = get_tar_fname(data_dir, train_tar)
-
-tar = train_tar
-mask_fname_match, img_fname_match = [], {}
-for t in tar[1:]:
-    i_fname_match = []
-    for i in img_fname[t]:
-        i_matcher = os.path.splitext(os.path.basename(i))[0]
-        for m in mask_fname[1:]:
-            m_matcher = os.path.splitext(os.path.basename(m))[0]
-            if 'mask' not in m_fname_split[:4]:
-                continue
-            elif i_matcher == m_matcher[-len(i_matcher)+1:]:
-                mask_fname_match.append(m)
-                i_fname_match.append(i)
-    img_fname_match[t] = i_fname_match
 '''
 
-'''
-# 안 쓸 예정
-tar = train_tar
-mask_fname_match = []
-xs, ys = [], []
-for t in range(len(tar)):
-    tar_fname = os.path.join(data_dir, tar[t] + '.tar.gz')
-    if t == 0:
-        mask_tar = tarfile.open(tar_fname)
-        mask_fname = tar_open.getnames()
-    else:
-        img_tar = tarfile.open(tar_fname)
-        img_fname = tar_open.getnames()
 
-    for i in img_fname:
-        matcher = os.path.splitext(os.path.basename(i))[0]  # i[len(tar[t])+1:-4]
-        for m in mask_fname:
-            if '/mask_' + matcher + '.png' in m:
-                mask_fname_match.append(m)
-                img_fname_match.append(i)
-
-    print(len(mask_fname_match), len(img_fname_match))
-
-    for i in img_fname_match:
-        img = img_tar.extractfile(i)
-
-        x = Image.open(img)
-        x = np.array(x)
-        xs.append(x)
-'''
-
-# mask image open here
-'''
-# 노쓸모
-mask_tar_fname = './dataverse_files/' + train_tar[0] + '.tar.gz'
-mask_tar = tarfile.open(mask_tar_fname)
-mask_fname = mask_tar.getnames()
-
-img_tar_fname = './dataverse_files/' + train_tar[1] + '.tar.gz'
-img_tar = tarfile.open(img_tar_fname)
-img_fname = img_tar.getnames()
-
-mask_match, img_match = [], []
-for n in img_fname:
-    matcher = n[len(train_tar[1])+1:-4]
-    for m in mask_fname:
-        if '/mask_' + matcher + '.png' in m:
-            mask_match.append(m)
-            img_match.append(n)
-'''
-
-'''name_match = []
-matcher = img_fname[1][len(train_tar[1])+1:-4]
-if matcher in mask_fname:
-    name_match.append(matcher)'''
-
-ori_size = (216, 216)
-img_size = (72, 72)
-img_fname_match.sort()
-
-def load_images(is_train, length, ori_size, img_size):
-    xs = []
-
-    for i in range(1): # range(len(img_tar)):
-        for j in range(length):  # img_fname[i]:
-            if is_train:
-                img = img_tar[2].extractfile(img_fname_match[j]) # img_tar랑 img_fname_folder 명이랑 매치 -> dict
-            else:
-                img = mask_tar.extractfile(mask_fname_match[j])  # mask_tar and mask_fname
-            x = Image.open(img)
-            '''    
-    for f in img_fname:
-        img = img_tar.extractfile(f)
-    
-        x = Image.open(img)
-            '''
-            # Preprocessing (crop and normalize)
-            width, height = x.size
-            cx = int(width / 2)
-            cy = int(height / 2)
-
-            roi = (cx - ori_size[0] / 2, cy - ori_size[1] / 2, cx + ori_size[0] / 2, cy + ori_size[1] / 2)
-
-            x = x.crop(roi)
-            #y = y.crop(roi)
-
-            x = x.resize(img_size)
-
-            x = np.asarray(x, dtype='float32') / 255.0
-            #y = np.asarray(y, dtype='float32') / 255.0
-
-            # crop -> resize, increase sample size
-
-            xs.append(x)
-            #ys.append(y)
-    return xs
-
-xs = load_images(True, 70, ori_size, img_size)
-ys = load_images(False, 70, ori_size, img_size)
-x_train = np.asarray(xs)
-y_train = np.asarray(ys)
-
-
-
+x_train, y_train = load_images(True, ori_size, img_size)  # 641 images
+x_valid, y_valid = load_images(False, ori_size, img_size)  # 245 images with pathologist1
 
 # Construct U-Net model
 # *** input shape
@@ -281,4 +233,4 @@ u_net.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['a
 u_net.fit(x_train, y_train, epochs=epochs)
 
 # Test the model with test set
-#u_net.evaluate(x_valid, y_valid, verbose=2)
+u_net.evaluate(x_valid, y_valid, verbose=2)
