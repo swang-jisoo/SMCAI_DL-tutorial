14bd4738de3c4c7f8884b016c790be57daae42d6
swang.jislee <swang.jislee@gmail.com>
2021-01-26 18:59:36 +0900

    edit to run all DWI images in dropbox

--- stroke_unet.py
+++ stroke_unet.py
@@ -24,106 +24,38 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, conca
 
 
 # parameters
+is_DWI_only = False
+root_dir = 'C:/Users/SMC/Dropbox/ESUS_ML'
+DCMv_dir = ['DCM_gtmaker_v2_release', 'DCM_gtmaker_v3', 'DCM_gtmaker_v5']
+
 rndcrop_size = (96, 96)
 resize_size = rndcrop_size
 output_size = 1  # binary segmentation
-learning_rate = 0.00001
-batch_size = 2
+learning_rate = 0.0005
+batch_size = 3
 epochs = 150
 
 # Results
-# ==> input size: 96*96, learning rate: 0.00001, batch size: 2, epochs: 150; dice: 0.26 ~ 0.33
+# ==> data: v5 DWI, input size: 96*96, learning rate: 0.00001, batch size: 2, epochs: 150; dice: 0.26 ~ 0.33
 # Memory allocation: rescale the image size and reduce the depth of the network
 # Imbalance: 90% of images in the dataset don't have a mask with clearly delineated lesions --> resample the datasets
 # Imbalance: Lesions take up a small portion of the entire image --> change accuracy/crossentropy to dice score/loss
 # Convergence optimization: failed to converge --> lower learning rate & batch size, higher epochs
 
+# ==> data: all DWI, input size: 96*96, learning rate: 0.00001, batch size: 2, epochs: 150; dice: ~ 0.12
+# ==> data: all DWI, input size: 96*96, learning rate: 0.00005, batch size: 2, epochs: 150; dice: 0.51
+# ==> data: all DWI, input size: 96*96, learning rate: 0.0001, batch size: 2, epochs: 150; dice: 0.66
+# ==> data: all DWI, input size: 96*96, learning rate: 0.0005, batch size: 3, epochs: 150; dice: 0.72
+# ==> data: all DWI, input size: 96*96, learning rate: 0.0005, batch size: 4, epochs: 150; dice: 0.65
+# ==> data: all DWI, input size: 96*96, learning rate: 0.0005, batch size: 3, epochs: 200; dice: 0.70
+
 
 # Define necessary functions
-'''
-# with images in v5/GT and v5/input folder
-def match_fname(mask_dir, img_dir):
-    """ Return the list of file names that exist in both image and mask folders """
+def get_matched_fpath(is_DWI_only, folder_dir):
+    """ Return a list of path of input images containing 'DWI' in the 'input' folder under the given directory. """
     # Get file names in the folder
-    (_, _, mask_f) = next(os.walk(mask_dir))
-    (_, _, img_f) = next(os.walk(img_dir))
-
-    mask_f.sort()
-    img_f.sort()
-
-    # Filter the file name that exists in both image and mask folders
-    fname = []
-    for m in mask_f:
-        m_base = os.path.splitext(m)[0]
-        for i in img_f:
-            i_base = os.path.splitext(i)[0]
-            if m_base != i_base:
-                continue
-            fname.append(m_base)
-
-    return fname
-
-
-def load_images(fname, rndcrop_size):
-    """ Load the numpy array of preprocessed image and mask datasets """
-    xs, ys = [], []
-    for f in fname:
-        # Load and rescale the input images
-        x = dcmread(os.path.join(img_dir, f + '.dcm')).pixel_array
-        x = zoom(x, rndcrop_size[0] / x.shape[0])
-        x = x.astype('float32') / 2048.0  # normalization
-        
-        # Load and rescale the mask images
-        y = Image.open(os.path.join(mask_dir, f + '.png')).convert('L')  # from rgb to greyscale
-        y = y.resize(rndcrop_size, resample=Image.BICUBIC)  # default resample = PIL.Image.BICUBIC
-        # Make an index for the part of lesions as 1
-        # (image thresholding) resize the image first and then apple thresholding
-        y = y.point(lambda p: p > 0.5)  # *** 0.5 due to BICUBIC
-        y = np.asarray(y, dtype='float32')
-
-        # Collect only the masks where lesions are clearly delineated
-        if y.max() == 0.:
-            continue
-
-        """
-        # Center crop the image
-        w_start = (x.shape[0] // 2) - (rndcrop_size[0] // 2)
-        h_start = (x.shape[1] // 2) - (rndcrop_size[1] // 2)
-        x = x[w_start:w_start + rndcrop_size[0], h_start:h_start + rndcrop_size[1]]
-        y = y[w_start:w_start + rndcrop_size[0], h_start:h_start + rndcrop_size[1]]
-        """
-        
-        xs.append(x)
-        ys.append(y)
-
-    xs = np.asarray(xs)
-    ys = np.asarray(ys)
-
-    return xs, ys
-  
-    
-def show_img(img_set, ncol, nrow):
-    """ Plot the list of images consisting of input image, mask, and predicted result """
-    assert nrow % len(img_set) == 0
-    num_imgs = ncol * nrow
-
-    fig = plt.figure(figsize=(8, 8))
-    # rnd_idx = [random.randint(0, len(img_set[0])) for _ in range(int(num_imgs / len(img_set)))]
-
-    for n in range(num_imgs):
-        fig.add_subplot(nrow, ncol, n+1)
-        px, py = int(n % ncol), int(n // ncol)
-        i, j = py % len(img_set), px + (ncol * (py // len(img_set)))
-        plt.imshow(img_set[i][j])
-
-    return plt.show()
-'''
-
-
-# with images in DCM & input folder
-def match_fname(mask_dir, img_dir):
-    """ Return the list of file names that exist in both image and mask folders """
-    # Get file names in the folder
+    mask_dir = os.path.join(folder_dir, 'GT')
+    img_dir = os.path.join(folder_dir, 'input')
     (_, _, mask_f) = next(os.walk(mask_dir))
     (_, _, img_f) = next(os.walk(img_dir))
 
@@ -135,18 +67,24 @@ def match_fname(mask_dir, img_dir):
         if 'DWI' in i:
             f_dwi = os.path.splitext(i)[0]
             f_adc = f_dwi[:f_dwi.index('DWI')] + 'ADC' + f_dwi[f_dwi.index('DWI') + 3:]
-            if (f_adc + '.tiff' in img_f) and (f_dwi + '.png' in mask_f):
-                fname_dwi.append(f_dwi)
+            if is_DWI_only and (f_dwi + '.png' in mask_f):
+                # returns file names containing 'DWI', which has a mask
+                fname_dwi.append(os.path.join(img_dir, i))
+            elif (not is_DWI_only) and (f_adc + '.dcm' in img_f) and (f_dwi + '.png' in mask_f):
+                # returns file names containing 'DWI', which has a corresponding name containing 'ADC' and has a mask
+                fname_dwi.append(os.path.join(img_dir, i))
 
-    return fname_dwi
+    return fname_dwi  # {folder_dir: fname_dwi}
 
 
-def load_images(fname_dwi, rndcrop_size):
+def load_images(is_DWI_only, fname_dwi, rndcrop_size):
     """ Load the numpy array of preprocessed image and mask datasets """
     xs, ys = [], []
     for f in fname_dwi:
         # Load and rescale the mask images
-        y = Image.open(os.path.join(mask_dir, f + '.png')).convert('L')  # from rgb to greyscale
+        f_mask = os.path.join(os.path.dirname(os.path.dirname(f)), 'GT',
+                              os.path.splitext(os.path.basename(f))[0] + '.png')
+        y = Image.open(f_mask).convert('L')  # from rgb to greyscale
         y = y.resize(rndcrop_size, resample=Image.BICUBIC)  # default resample = PIL.Image.BICUBIC
         # Make an index for the part of lesions as 1
         # (image thresholding) resize the image first and then apple thresholding
@@ -158,19 +96,39 @@ def load_images(fname_dwi, rndcrop_size):
             continue
 
         # Load and rescale the input images
-        f_adc = f[:f.index('DWI')] + 'ADC' + f[f.index('DWI') + 3:]
-        x_adc = Image.open(os.path.join(img_dir, f_adc + '.tiff'))
-        x_adc = x_adc.resize(rndcrop_size, resample=Image.BICUBIC)
-        x_adc = np.array(x_adc, dtype='float32')
-        # *** normalization
+        if is_DWI_only:
+            x = dcmread(f).pixel_array
+            x = zoom(x, rndcrop_size[0] / x.shape[0])  # rescale
+            x = x.astype('float32') / 2048.0  # normalization
+        else:
+            f_base = os.path.basename(f)
+            f_adc = os.path.join(os.path.dirname(f),
+                                 f_base[:f_base.index('DWI')] + 'ADC' + f_base[f_base.index('DWI') + 3:])
+            x_adc = dcmread(f_adc).pixel_array
+            x_adc = zoom(x_adc, rndcrop_size[0] / x_adc.shape[0])  # rescale
+            x_adc = x_adc.astype('float32') / 2048.0  # normalization
 
-        x_dwi = Image.open(os.path.join(img_dir, f + '.tiff'))
-        x_dwi = x_dwi.resize(rndcrop_size, resample=Image.BICUBIC)
-        x_dwi = np.array(x_dwi, dtype='float32')
-        # *** normalization
+            x_dwi = dcmread(f).pixel_array
+            x_dwi = zoom(x_dwi, rndcrop_size[0] / x_dwi.shape[0])  # rescale
+            x_dwi = x_dwi.astype('float32') / 2048.0  # normalization
 
-        # Concatenate ADC and DWI image
-        x = np.concatenate((x_adc[:, :, np.newaxis], x_dwi[:, :, np.newaxis]), axis=-1)
+            """
+            # NOT TIFF FILES
+            # Load and rescale the input images
+            f_adc = f[:f.index('DWI')] + 'ADC' + f[f.index('DWI') + 3:]
+            x_adc = Image.open(os.path.join(img_dir, f_adc + '.tiff'))
+            x_adc = x_adc.resize(rndcrop_size, resample=Image.BICUBIC)
+            x_adc = np.array(x_adc, dtype='float32')
+            # *** normalization
+    
+            x_dwi = Image.open(os.path.join(img_dir, f + '.tiff'))
+            x_dwi = x_dwi.resize(rndcrop_size, resample=Image.BICUBIC)
+            x_dwi = np.array(x_dwi, dtype='float32')
+            # *** normalization
+            """
+
+            # Concatenate ADC and DWI image
+            x = np.concatenate((x_adc[:, :, np.newaxis], x_dwi[:, :, np.newaxis]), axis=-1)
 
         xs.append(x)
         ys.append(y)
@@ -181,24 +139,28 @@ def load_images(fname_dwi, rndcrop_size):
     return xs, ys
 
 
-def show_img(img_set, ncol, nrow):
+def show_img(is_DWI_only, img_set, ncol, nrow):
     """ Plot the list of images consisting of input image, mask, and predicted result """
-    assert nrow % (len(img_set) + 1) == 0
     num_imgs = ncol * nrow
-
     fig = plt.figure(figsize=(8, 8))
     # rnd_idx = [random.randint(0, len(img_set[0])) for _ in range(int(num_imgs / len(img_set)))]
 
     for n in range(num_imgs):
         fig.add_subplot(nrow, ncol, n+1)
         px, py = int(n % ncol), int(n // ncol)
-        i, j = py % len(img_set), px + (ncol * (py // len(img_set)))
-        if i == 0:
-            plt.imshow(img_set[0][j][:, :, i])
-        elif i == 1:
-            plt.imshow(img_set[0][j][:, :, i])
-        else:
+        if is_DWI_only:
+            assert nrow % len(img_set) == 0
+            i, j = py % len(img_set), px + (ncol * (py // len(img_set)))
             plt.imshow(img_set[i][j])
+        else:
+            assert nrow % (len(img_set) + 1) == 0
+            i, j = py % len(img_set), px + (ncol * (py // len(img_set)))
+            if i == 0:
+                plt.imshow(img_set[0][j][:, :, i])
+            elif i == 1:
+                plt.imshow(img_set[0][j][:, :, i])
+            else:
+                plt.imshow(img_set[i][j])
 
     return plt.show()
 
@@ -229,68 +191,52 @@ def dice_loss(y_true, y_pred):
 
 
 # Load the dataset
-'''
-# with images in v5/GT and v5/input folder
-# directory structure
-#   + stroke_dcm
-#       + v2, v3, v5
-#           + DCM: input image (ADC & DWI); tiff files
-#           + GT: mask; png files
-#           + input: input image (DWI); dcm files
+# directory structure (Dropbox)
+#   + ESUS_ML
+#       + DCM_gtmaker_v2_release
+#           + DCM: 603 input images (ADC & DWI); tiff files
+#           + GT: 603 mask images (ADC & DWI); png files
+#           + input: 74 input images (DWI); dcm files --> 0 masks with lesion
+#       + DCM_gtmaker_v3
+#           + DCM: 32,863 input images (ADC & DWI); tiff files --> local: 0
+#           + GT: 32,863 mask images (ADC & DWI); png files --> local: 2,130
+#           + input: 242 input images (ADC & DWI); dcm files --> 45 masks with lesion
+#       + DCM_gtmaker_v5
+#           + DCM: 7,216 input images (ADC & DWI); tiff files --> local: 7,135
+#           + GT: 6,737 mask images (ADC & DWI); png files
+#           + input: 574 input images (DWI); dcm files --> 525 non error images; 53 masks with lesion
 
-mask_dir = 'D:/PycharmProjects/stroke_dcm/v5/GT'
-img_dir = 'D:/PycharmProjects/stroke_dcm/v5/input'
+# Get DWI image paths
+fname_dwi = []  # 720 = 74+121+525
+for dv in DCMv_dir:
+    if dv == DCMv_dir[-1]:
+        # Note: some of images are excluded due to the error below:
+        # AttributeError: 'FileMetaDataset' object has no attribute 'TransferSyntaxUID'
+        # e.g.
+        # x_np[71] = './stroke_dcm/input\\281SVODWI0001.dcm'
+        # x_np[224] = './stroke_dcm/input\\286SVODWI0001.dcm'
+        fname_cut = get_matched_fpath(is_DWI_only, os.path.join(root_dir, dv))  # 574
+        fname_dwi += fname_cut[:71] + fname_cut[97:224] + fname_cut[247:]  # 525
+    else:
+        fname_dwi += get_matched_fpath(is_DWI_only, os.path.join(root_dir, dv))
 
-# with v5/GT and v5/input
-fname = match_fname(mask_dir, img_dir)
-fname_cut = fname[:71] + fname[97:224] + fname[247:]
-# Note: some of images are excluded due to the error below:
-# AttributeError: 'FileMetaDataset' object has no attribute 'TransferSyntaxUID'
-# e.g.
-# x_np[71] = './stroke_dcm/input\\281SVODWI0001.dcm'
-# x_np[224] = './stroke_dcm/input\\286SVODWI0001.dcm'
-"""
-# files with lesion segmented mask
-fname_cut = ['0102LAADWI0012', '0102LAADWI0013', '0102LAADWI0014', '0102LAADWI0015', '0102LAADWI0016',
-             '0102LAADWI0017', '0102LAADWI0018', '0102LAADWI0019', '0102LAADWI0020', '0102LAADWI0021',
-             '0102LAADWI0022', '0102LAADWI0023', '0120LAADWI0013', '0120LAADWI0014', '0120LAADWI0015',
-             '0120LAADWI0016', '280SVODWI0003', '280SVODWI0014', '281SVODWI0018', '282SVODWI0006',
-             '283SVODWI0017', '283SVODWI0018', '284SVODWI0007', ... etc]
-"""
 # Load the preprocessed image and mask datasets
-x_all, y_all = load_images(fname_cut, rndcrop_size)
+x_all, y_all = load_images(is_DWI_only, fname_dwi, rndcrop_size)  # 98 = 0+45+53
 
 # Shuffle the dataset and split into train and test sets
 # x_train, y_train = shuffle_ds(x_all[:-52], y_all[:-52])
 # x_valid, y_valid = x_all[-52:], y_all[-52:]  # (last 2 patients)
 
-# Shuffle the dataset and split train and test sets
-# (the entire dataset only includes the images with clearly delineated lesions on the mask)
+# Shuffle the dataset and split into train and test sets
+# The entire dataset only includes the images with clearly delineated lesions on the mask
+# The last 5 images belongs to one patient who does not included in the train set (DCM_gtmaker_v5\GT\299SVODWI0013 ~ 17)
 x_train, y_train = shuffle_ds(x_all[:-5], y_all[:-5])
 x_valid, y_valid = x_all[-5:], y_all[-5:]
-'''
-
-# with images in DCM & input folder
-# directory structure
-#   + stroke_dcm
-#       + v2, v3, v5
-#           + DCM: input image (ADC & DWI); tiff files
-#           + GT: mask; png files
-#           + input: input image (DWI); dcm files
-
-mask_dir = 'D:/PycharmProjects/stroke_dcm/v2/GT'
-img_dir = 'D:/PycharmProjects/stroke_dcm/v2/DCM'
-
-# Get the file names to read
-fname_dwi = match_fname(mask_dir, img_dir)
-
-# Load the preprocessed image and mask datasets
-x_all, y_all = load_images(fname_cut, rndcrop_size)
-
 
 
 # Construct U-Net model
-input_tensor = Input(shape=resize_size + (1,), name='input_tensor')
+channel = 1 if is_DWI_only else 2
+input_tensor = Input(shape=resize_size + (channel,), name='input_tensor')
 
 # Contracting path
 cont1_1 = Conv2D(32, 3, padding='same',
@@ -355,9 +301,13 @@ img_arg = img_arg.astype('float32')
 img_arg = img * 255
 '''
 
+# print(len(fname_dwi))
+# print(len(x_all), len(y_all))
+
 img_set = [x_valid, y_valid, img]
-ncol, nrow = 5, 3
-show_img(img_set, ncol, nrow)
+ncol = 5
+nrow = 3 if is_DWI_only else 4
+show_img(is_DWI_only, img_set, ncol, nrow)
 
 '''
 fpath = './stroke_dcm/input/0102LAADWI0005.dcm'
