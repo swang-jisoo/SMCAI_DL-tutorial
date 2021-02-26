03ac892b2f23904fdbc0f7fc9b1832f1f8e43b40
swang.jislee <swang.jislee@gmail.com>
2021-02-26 15:27:20 +0900

    update

--- 2_classification_cifar10_vgg19.py
+++ 2_classification_cifar10_vgg19.py
@@ -1,54 +1,54 @@
 #####
+# Image Classification
+
 # dataset: CIFAR-10
-# The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class.
-# training: total 50000 images, divided into 5 batches, each with 10000 images
-# (the entire training set contain exactly 5000 images from each class; some batch may contain more images from one
-# class than other)
-# test: total 10000 images in one batch (1000 randomly-selected images from each class)
+# description: a labeled set of colored images
+# images: shape=(32, 32, 3)
+# labels: num_classes=10 (0=airplane, 1=car, 2=bird, 3=cat, 4=deer, 5=dog, 6=frog, 7=horse, 8=ship, and 9=truck)
+# total_num_examples=60000
+# split: training=50000 (5000 images per class), test=10000 (1000 randomly-selected images per class)
+# ref: https://www.cs.toronto.edu/~kriz/cifar.html
 
 # model: VGG19
 # ref. paper: VERY DEEP CONVOLUTIONAL NETWORKS FOR LARGE-SCALE IMAGE RECOGNITION
-
-# Notation
-# ***: Questions or further information to check are remained
-# NOTE: if the code is modified, be aware of the corresponding codes
 #####
+
 
 # Import necessary libraries
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from tensorflow.keras import Input, Model
 
-# Fix the gpu memory issue
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.3  # NOTE: <=0.3 with GTX 1050 (2GB)
-session = tf.compat.v1.Session(config=config)
+# Set the value of hyper-parameters
+IMG_ROWS, IMG_COLS, IMG_CHANNELS = 32, 32, 3
+IMG_SHAPE = (IMG_ROWS, IMG_COLS, IMG_CHANNELS)
+CLASSES = 10
+EPOCHS = 20
+LEARNING_RATE = 0.0001
+BATCH_SIZE = 16
+VERBOSE = 1
 
-# Load cifar10 dataset
+# Results by hyper-parameters
+# ==> EPOCHS=20; LEARNING_RATE=0.0001; BATCH_SIZE=16; loss: 0.9532 - accuracy: 0.8017
+
+
+# Load the dataset
 cifar10 = tf.keras.datasets.cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 # Preprocess the dataset
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# Data-type conversion
 x_train = x_train.astype("float32")
 x_test = x_test.astype("float32")
-y_train_onehot = tf.squeeze(tf.one_hot(y_train, 10), axis=1)
-y_test_onehot = tf.squeeze(tf.one_hot(y_test, 10), axis=1)
+# Normalization
+x_train, x_test = x_train / 255.0, x_test / 255.0
+# One-hot encoding
+y_train = tf.keras.utils.to_categorical(y_train, CLASSES)
+y_test = tf.keras.utils.to_categorical(y_test, CLASSES)
 
-# Set the value of hyper-parameters
-epochs = 20
-learning_rate = 0.0001
-batch_size = 16
-# upsampling_size = (2,2)
 
-# Results by hyper-parameters
-# ==> learning rate: 0.0001; Epoch: 20; batch size: 16; loss: 0.9532 - accuracy: 0.8017
-
-# Initiate a VGG16 architecture
-input_tensor = Input(shape=(32, 32, 3), dtype='float32', name='input')
-# Rescale image (up-sampling) for better performance
-# NOTE: change the next layer's input
-# upsampling = tf.keras.layers.UpSampling2D(size=upsampling_size, name='upsampling')(input_tensor)
+# Build a VGG19 architecture
+input_tensor = Input(shape=IMG_SHAPE, dtype='float32', name='input')
 
 # block 1
 conv1_1 = Conv2D(64, 3, activation='relu', padding='same', name='conv1-1')(input_tensor)
@@ -82,20 +82,25 @@ flatten = Flatten(name='flatten')(maxpool5)
 # fc1 = Dense(4096, activation='relu', name='fc1')(flatten)
 # fc2 = Dense(2048, activation='relu', name='fc2')(fc1)
 fc3 = Dense(256, activation='relu', name='fc3')(flatten)  # NOTE: check input
-output_tensor = Dense(10, activation='softmax', name='output')(fc3)
+output_tensor = Dense(CLASSES, activation='softmax', name='output')(fc3)
 
 # Create a model
 vgg19 = Model(input_tensor, output_tensor, name='vgg19')
 vgg19.summary()
 
 # Compile the model
-optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
 vgg19.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
 
+# Fix the gpu memory issue (failed to create cublas handle: CUBLAS_STATUS_ALLOC_FAILED)
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.3  # NOTE: <=0.3 with GTX 1050 (2GB)
+session = tf.compat.v1.Session(config=config)
+
 # Train the model to adjust parameters to minimize the loss
-vgg19.fit(x_train, y_train_onehot, batch_size=batch_size, epochs=epochs)
+vgg19.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE)
 
 # Predict the model with test set
-vgg19.evaluate(x_test, y_test_onehot, verbose=2)
+vgg19.evaluate(x_test, y_test, verbose=VERBOSE)
